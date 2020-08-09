7bebc8b3e8c0a5324733e89d18c146829c82c01b
alfredjmg <alfredomujicagodoy@gmail.com>
2020-08-08 20:17:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './Components/Header';
+import Formualario from './Components/Formulario';
+import Resumen from './Components/Resumen';
+import Resultado from './Components/Resultado';
+import Spinner from './Components/Spinner';
+
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+    background-color: #fff;
+    padding: 3rem;
+`;
 
 function App() {
+
+  const [ resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: ''
+    }
+  });
+
+  const [ cargando, guardarCargando] = useState(false);
+
+  // extraer datos
+  const {datos} = resumen;
+  const {cotizacion} = resumen;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Header
+      titulo='Cotizador de Seguros'
+      />
+      <ContenedorFormulario>
+        <Formualario
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+
+        {cargando ? <Spinner/> : null}
+
+        <Resumen
+          datos={datos}
+        />
+
+        {!cargando
+          ?
+          <Resultado
+            cotizacion={cotizacion}
+          />
+          :
+          null
+        }
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
---
+++ src/helper.js
@@ -0,0 +1,36 @@
+// obtener la diferencia de años
+export function obtenerDiferenciaYear(Year){
+    return new Date().getFullYear() - Year;
+}
+
+// calcular marca
+export function calcularMarca(marca){
+
+    let incremento;
+
+    switch(marca){
+        case 'europeo':
+        incremento = 1.30;
+        break;
+        case 'americano':
+        incremento = 1.15;
+        break;
+        case 'asiatico':
+        incremento = 1.05;
+        break;
+        default:
+            break;
+    }
+
+    return incremento;
+}
+
+// calcula el tipo de seguro
+export function obtenerPlan(plan){
+    return (plan === 'basico') ? 1.20 : 1.50;
+}
+
+// Muestra la primer letra mayuscula
+export function primerMayuscula( texto) {
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
